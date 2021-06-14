34a7add921e363c630d467f0543b8b0cde9085a6
Catherine Li <catherine.uk.20@gmail.com>
2021-06-14 09:59:15 +0100

    adding toggle to price display

--- src/components/StoreItem.js
+++ src/components/StoreItem.js
@@ -1,13 +1,18 @@
 
-
+function displayPrice(id){
+  document.getElementById(id+"price").style.display === "block"
+  ? document.getElementById(id+"price").style.display = "none"
+  : document.getElementById(id+"price").style.display = "block"
+}
 function StoreItem({item, addItemToCart}){
   const{ id, name, price} =item
+
     return (
-        <li>
+        <li onClick={()=>displayPrice(id)}>
   <div className="store--item-icon">
     <img src={`assets/icons/${id}.svg`} alt={name} />
   </div>
-  <span>£{price} /each</span>
+  <span id={id+"price"} style={{display:"none"}}>£{price} /each</span>
   <button onClick={()=>addItemToCart(id)}>Add to cart</button>
 </li>
     )
